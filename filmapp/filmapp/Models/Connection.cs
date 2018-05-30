e2c3f598e1a57fcc069fb7a38c2cb850e974b80c
R221 <rcjames1111@gmail.com>
2018-05-29 22:35:30 -0400

    replaced all instances of item with connection git status

---
+++ filmapp/filmapp/Models/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace filmapp.Models
+{
+    public class Connection
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Revenue { get; set; }
+    }
+}
